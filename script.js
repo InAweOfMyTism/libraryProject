9f57edf389f8035f42de7ac5a179cffa286dafcf
InAweofMyTism <inaweofmytism@gmail.com>
2024-09-04 14:08:37 -0400

    add function to display book info as html elements

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const myLibrary = [];
+const bookshelf = document.getElementById('container');
+
 
 function Book(title, author, genre, wordCount) {
     this.title = title;
@@ -18,6 +20,46 @@ function Book(title, author, genre, wordCount) {
         else 
             return `${title}, by ${author}, ${wordCount} words, unread`;
             
+    }
+    this.displayBook = function () {
+
+
+        this.bookCard = document.createElement('div');
+        this.bookCard.classList.add('book card');
+        
+
+        this.cardTitle = document.createElement('h1');
+        this.cardTitle.classList.add('title');
+        this.cardTitle.textContent = this.title;
+
+        this.cardImg = document.createElement('img');
+        this.cardImg.classList.add('cover');
+
+        
+        this.cardAuthor = document.createElement('h2');
+        this.cardAuthor.classList.add('author');
+        this.cardAuthor.textContent = `by ${this.author}`;
+
+        
+        this.cardGenre = document.createElement('h3');
+        this.cardGenre.classList.add('genre');
+        this.cardGenre.textContent = this.genre;
+        
+        this.cardWordCount = document.createElement('h3');
+        this.cardWordCount.classList.add('wordCount');
+        this.cardWordCount.textContent = `${this.wordCount} words`
+        
+        this.readButton = document.createElement('button');
+        this.readButton.classList.add('read_toggle');
+
+
+        this.bookCard.appendChild('cardTitle');
+        this.bookCard.appendChild('cardAuthor');
+        this.bookCard.appendChild('cardGenre');
+        this.bookCard.appendChild('cardWordCount');
+        this.bookCard.appendChild('readButton');
+
+        bookshelf.appendChild('bookCard');
     }
 }
 
